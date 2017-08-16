414a552251ea7f9e0bb8ff73a2fa0e4565dfa98c
jarehec <bone1198@gmail.com>
2017-08-16 15:46:40 +0000

    fixed datetime attribute syntax

--- models/base_model.py
+++ models/base_model.py
@@ -19,8 +19,8 @@ class BaseModel:
     """attributes and functions for BaseModel class"""
 
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(Datetime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(Datetime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime(default=datetime.utcnow()), nullable=False)
+    updated_at = Column(DateTime(default=datetime.utcnow()), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """instantiation of new BaseModel Class"""
