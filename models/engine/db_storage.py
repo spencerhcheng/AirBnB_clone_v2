f1e1b29b64fc3958c22fc51209e186d9d562d7a0
jarehec <bone1198@gmail.com>
2017-08-17 01:54:38 +0000

    updated dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
+import sys
+import os
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+from models.base_model import BaseModel, Base
 
-if __name__ == '__main__':
-    import sys
-    import os
-    from sqlalchemy import create_engine, MetaData
-    from sqlalchemy.orm import sessionmaker
-    from base_model import BaseModel, Base
+all_objs = ['User', 'State', 'City', 'Amenity', 'Place', 'Review']
 
+class DBStorage:
     __engine = None
     __session = None
-    all_objs = [User, State, City, Amenity, Place, Review]
 
-    def init(self): 
+    def init(self):
         self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost{}'.format(os.environ.get('HBNB_MYSQL_USER'), os.environ.get('HBNB_MYSQL_PWD'), os.environ.get('HBNB_MYSQL_HOST'), os.environ.get('HBNB_MYSQL_DB')))
         
         Base.metadata.create_all(self.__engine)
@@ -27,7 +27,7 @@ if __name__ == '__main__':
             for item in all_objs:
                 self.__session.query(item)
         class_dict = self.__session.query(cls)
-        return dict{class_dict}
+        return dict(class_dict)
 
     def new(self, obj):
         self.__session.add(obj)
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 
     def delete(self, obj=None):
         if obj is not None:
-            del obj
+            self.__session.delete(obj)
 
     def reload(self):
-        refresh(self, obj)
+        self.__session.refresh(obj)
