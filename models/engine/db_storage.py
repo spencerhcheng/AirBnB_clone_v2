73cb778ec297b40fdde5e9c41c82e184c704e69c
Spencer <spencerhcheng@gmail.com>
2017-08-17 00:39:01 +0000

    Pushing fixes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,15 +14,16 @@ if __name__ == '__main__':
 
     def init(self): 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost{}'.format(os.environ.get('HBNB_MYSQL_USER'), os.environ.get('HBNB_MYSQL_PWD'), os.environ.get('HBNB_MYSQL_HOST'), os.environ.get('HBNB_MYSQL_DB')))
-        Base.metadata.create_all(__engine)
+        
+        Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine)
-        __session = Session()
+        self.__session = Session()
 
-        if (os.environ.get('HBNB_ENV') == 'test':
+        if os.environ.get('HBNB_ENV') == 'test':
             MetaData.drop_all()
 
     def all(self, cls=None):
-        if cls == None:
+        if cls is None:
             for item in all_objs:
                 self.__session.query(item)
         class_dict = self.__session.query(cls)
