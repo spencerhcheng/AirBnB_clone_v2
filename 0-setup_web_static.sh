bb5a00541401219c4afc2a4bcb63bb118c59b6ed
Spencer <spencerhcheng@gmail.com>
2017-08-22 02:25:34 +0000

    Change sed to echo. Fix sed command into default

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,8 +8,8 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test
 sudo touch /data/web_static/releases/test/index.html
-sudo sed -i "1i <html>\n<header><title>This is title</title></header>\n<body>\nHello Holberton\n</body>\n</html>"  /data/web_static/releases/test/index.html
+sudo echo "<html><head></head><body>Hello Holberton</body></html>" | sudo tee --append /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown ubuntu:ubuntu /data/
-sudo sed -i "37 i \ \t location /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enabled/default
+sudo sed -i "37 i \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx reload
