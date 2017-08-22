1b0dd9f3240e50cc9fcdb3843a32baaa65766257
Spencer <spencerhcheng@gmail.com>
2017-08-22 02:44:41 +0000

    Recursive permissions with chown -R

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,6 @@ sudo mkdir -p /data/web_static/releases/test
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html><head></head><body>Hello Holberton</body></html>" | sudo tee --append /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "37 i location /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enabled/default
-sudo service nginx start
-sudo service nginx reload
+sudo service nginx restart
