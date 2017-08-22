20b18fc4d28563b313518456c1f669e68c61d58e
Spencer Cheng <spcheng0412@gmail.com>
2017-08-22 01:47:22 +0000

    0-setup_web_static: initial commit. Still not working

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
-apt update
-apt install -y nginx
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
-echo 'Hello World!' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-loc="location /hbnb_static {\n\talias /data/web_static/current;\n}"
-sed -i "38i $loc" /etc/nginx/sites-available/default
-service nginx start
-service nginx reload
+# Sets up web servers for deployment of `web_static`
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test
+sudo touch /data/web_static/releases/test/index.html
+sudo sed -i "1i <html>\n<header><title>This is title</title></header>\n<body>\nHello Holberton\n</body>\n</html>"  /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown ubuntu:ubuntu /data/
+sudo sed -i "37 i \ \t location /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enabled/default
+sudo service nginx reload
