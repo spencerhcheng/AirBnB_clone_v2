5c3e4e7afa698b063030c32698998618f64ec2eb
Spencer <spencerhcheng@gmail.com>
2017-08-22 02:42:12 +0000

    Remove tab in sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,6 @@
 # Sets up web servers for deployment of `web_static`
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo service nginx start
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
@@ -11,5 +10,6 @@ sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html><head></head><body>Hello Holberton</body></html>" | sudo tee --append /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown ubuntu:ubuntu /data/
-sudo sed -i "37 i \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enabled/default
-sudo service nginx restart
+sudo sed -i "37 i location /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enabled/default
+sudo service nginx start
+sudo service nginx reload
