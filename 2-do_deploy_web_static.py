6d0b7d594c643191370f7a6c1464823acfc403c2
Spencer <spencerhcheng@gmail.com>
2017-08-24 22:20:31 +0000

    2-do_deploy_web_static: first commit of working code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,21 +11,33 @@ import sys
 # Server username and hostname
 env.hosts = ['34.207.108.29', '34.229.54.8']
 
+
 def do_deploy(archive_path):
     if archive_path is None:
         return False
 
-    args = sys.argv[3]
-    arg = args.split('/')[1]
-    no_tgz = arg.split('.')[0]
+    arch_list = archive_path.split("/")
+    arch_name = arch_list[1]
+    arch_name_sans = arch_name.split('.')[0]
 
-    put('./versions/{}'.format(arg), '/tmp/{}'.format(arg))
-    run("sudo tar zxcf '/tmp/{} /data/web_static/releases/{}'.format(arg, no_tgz)")
-    local('sudo rm *.tgz ./versions', capture=False)
-    local('sudo unlink ./data/web_static/current')
-    local("sudo ln -s './data/web_static/releases/{}'.format(no_tgz) /data/web_static/current")
-
-    if not os.path.exists("./data/web_static/releases/{}".format(no_tgz)):
+    if not os.path.exists("{}".format(archive_path)):
+        print("Path doesn't exist!")
         return False
-    else:
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}'.format(arch_name_sans))
+        run('tar xzf /tmp/{} -C\
+            /data/web_static/releases/{}'.format(arch_name, arch_name_sans))
+        run('rm /tmp/{}'.format(arch_name))
+        run('mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}'.
+            format(arch_name_sans, arch_name_sans))
+        run('rm -rf /data/web_static/releases/{}/web_static'.
+            format(arch_name_sans))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}\
+            /data/web_static/current'.format(arch_name_sans))
         return True
+    except Exception as x:
+        print(x)
+        return False
