d87f5ece39fc6b1c28a626aa65665d49d78a912a
Spencer <spencerhcheng@gmail.com>
2017-09-05 23:10:49 +0000

    0-hello_route.py: initial commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # Starts a Flask web app
 from flask import Flask
-if __name__ == "__main__":
-	app = Flask(__name__)
-
-	@app.route('/')
-	
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+@app.route('/')
+def hello_hbnb():
+    return "Hello HBNB!"
+if (__name__) == "__name__":
+   app.run(host='0.0.0.0', port=5000)
