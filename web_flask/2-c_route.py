14c2d1486477607714e2024c4b3d50c5056d7aae
Spencer <spencerhcheng@gmail.com>
2017-09-06 22:41:26 +0000

    2-c_route: initial commit

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Starts flask web application
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def print_text(text):
+    return "C %s" % text.replace('_', ' ')
+if (__name__) == "__main__":
+    app.run(host='0.0.0.0', port=5000)
