b1ee1bdc52fa8720c969d146096993120b111ed9
Spencer <spencerhcheng@gmail.com>
2017-09-07 00:44:41 +0000

    Add reference to html file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Starts flask web application
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -30,5 +30,12 @@ def print_python(text="is cool"):
 def num(n):
     if type(n) is int:
         return ("{} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>')
+def num_template(n):
+   return render_template('5-number.html', n=n)
+
+
 if (__name__) == "__main__":
     app.run(host='0.0.0.0', port=5000)
