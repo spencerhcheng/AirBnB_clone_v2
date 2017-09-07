7e1a2d7cfb7a13618114fcf8508c48b885d484f2
Spencer <spencerhcheng@gmail.com>
2017-09-07 00:40:38 +0000

    5-number: initial commit

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# Starts flask web application
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def print_text(text):
+    return "C %s" % text.replace('_', ' ')
+
+
+@app.route('/python/<text>')
+@app.route('/python')
+def print_python(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def num(n):
+    if type(n) is int:
+        return ("{} is a number".format(n))
+if (__name__) == "__main__":
+    app.run(host='0.0.0.0', port=5000)
