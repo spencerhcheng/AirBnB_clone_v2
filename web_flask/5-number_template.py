1b83136107e5016c825144863a3e922b6b31d084
Spencer <spencerhcheng@gmail.com>
2017-09-07 01:43:43 +0000

    5-number_template: pep8 fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,7 +34,7 @@ def num(n):
 
 @app.route('/number_template/<int:n>')
 def num_template(n):
-   return render_template('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 if (__name__) == "__main__":
