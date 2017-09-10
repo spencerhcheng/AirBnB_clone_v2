27896bae8477e3e381a291240cdaaff93a46b206
Spencer <spencerhcheng@gmail.com>
2017-09-10 05:03:05 +0000

    7-states_list: change content to context

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# Starts flask web app to serve AirBnB content
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def rem_session(exception):
+    """
+    Removes current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/states_list')
+def states_hbnb():
+    """
+    returns all states in db
+    """
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
