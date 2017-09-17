b34b89abe5d8e6fbc0ca703a341842bccac886da
Spencer Cheng <spcheng0412@gmail.com>
2017-09-17 22:44:34 +0000

    8-cities_by_states: PEP8 fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,6 +5,7 @@ from models import storage
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.teardown_appcontext
 def rem_session(exception):
     """Removes current SQLAlchemy Session"""
@@ -16,6 +17,7 @@ def display_html():
     """List all State objects present in DBStorage"""
     return render_template('8-cities_by_states.html',
                            states_list=storage.all('State').values())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
